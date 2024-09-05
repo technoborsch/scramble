18c787ee73f28ff57f5b66ac734c41c180692178
n.nikiforov <nsnikiforov@spbaep.ru>
2024-09-05 15:25:09 +0300

    Acad printer - plotter settings now are applied via context manager to ensure settings safety

--- AcadPrinter.py
+++ AcadPrinter.py
@@ -23,44 +23,41 @@ class AcadPrinter:
 
     def __init__(self):
         self.acad = None
-        self.original_printer_config_path = None
         self._setup()
 
     def convert(self, dwg_path, output_path, doc_size):
         self._setup()
         self.acad.Application.Documents.Open(dwg_path)
         sleep(5)
+        with applied_plotter_settings(self.acad):
 
-        dwg = self.acad.ActiveDocument
-        plot = dwg.Plot
-        this_config = dwg.ActiveLayout
-        this_config.RefreshPlotDeviceInfo()
+            dwg = self.acad.ActiveDocument
+            plot = dwg.Plot
+            this_config = dwg.ActiveLayout
 
-        this_config.ConfigName = "DWG To PDF.pc3"
-        this_config.UseStandardScale = False
-        this_config.StandardScale = pyautocad.ACAD.acScaleToFit
-        this_config.PlotRotation = pyautocad.ACAD.ac0degrees
-        this_config.PlotType = pyautocad.ACAD.acExtents
-        this_config.RefreshPlotDeviceInfo()
-        this_config.CanonicalMediaName = config.PLOT_LIST_MAP[doc_size]
-        this_config.CenterPlot = True
-        this_config.PlotWithPlotStyles = True
-        this_config.StyleSheet = "akku-standard.ctb"
+            this_config.ConfigName = "DWG To PDF.pc3"
+            this_config.RefreshPlotDeviceInfo()
+            this_config.UseStandardScale = False
+            this_config.StandardScale = pyautocad.ACAD.acScaleToFit
+            this_config.PlotRotation = pyautocad.ACAD.ac0degrees
+            this_config.PlotType = pyautocad.ACAD.acExtents
+            this_config.RefreshPlotDeviceInfo()
+            this_config.CanonicalMediaName = config.PLOT_LIST_MAP[doc_size]
+            this_config.CenterPlot = True
+            this_config.PlotWithPlotStyles = True
+            this_config.StyleSheet = "akku-standard.ctb"
 
-        dwg.SetVariable("BACKGROUNDPLOT", 0)
+            dwg.SetVariable("BACKGROUNDPLOT", 0)
 
-        this_config.RefreshPlotDeviceInfo()
+            this_config.RefreshPlotDeviceInfo()
 
-        result = plot.PlotToFile(
-            output_path,
-            this_config.ConfigName
-        )
+            result = plot.PlotToFile(
+                output_path,
+                this_config.ConfigName
+            )
         return result
 
     def close_acad(self):
-        self.acad.Application.Preferences.Files.PrinterConfigPath = self.original_printer_config_path
-        self.acad.Application.Preferences.Files.PrinterDescPath = self.original_print_desc_path
-        self.acad.Application.Preferences.Files.PrinterStyleSheetPath = self.original_print_styles_path
         if self.acad.Application.Documents.Count != 0:
             for doc in self.acad.Application.Documents:
                 doc.Close(False)
@@ -68,12 +65,26 @@ class AcadPrinter:
 
     def _setup(self):
         self.acad = Autocad(True, False)
+
+
+class applied_plotter_settings:
+
+    def __init__(self, acad):
+        self.acad = acad
+
+    def __enter__(self):
         self.original_printer_config_path = deepcopy(self.acad.Application.Preferences.Files.PrinterConfigPath)
         self.original_print_desc_path = deepcopy(self.acad.Application.Preferences.Files.PrinterDescPath)
         self.original_print_styles_path = deepcopy(self.acad.Application.Preferences.Files.PrinterStyleSheetPath)
         self.acad.Application.Preferences.Files.PrinterConfigPath = plot_config_path
         self.acad.Application.Preferences.Files.PrinterDescPath = plot_printer_desc_path
         self.acad.Application.Preferences.Files.PrinterStyleSheetPath = plot_styles_path
+        self.acad.ActiveDocument.ActiveLayout.RefreshPlotDeviceInfo()
+
+    def __exit__(self, type_, value, traceback):
+        self.acad.Application.Preferences.Files.PrinterConfigPath = self.original_printer_config_path
+        self.acad.Application.Preferences.Files.PrinterDescPath = self.original_print_desc_path
+        self.acad.Application.Preferences.Files.PrinterStyleSheetPath = self.original_print_styles_path
 
 
 if __name__ == "__main__":
