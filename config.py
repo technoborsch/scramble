3f75b028cf0c62530b1f55e20eac8c06574b5fdb
n.nikiforov <nsnikiforov@spbaep.ru>
2024-10-02 14:57:35 +0300

    Add new instruction, new journal path

--- config.py
+++ config.py
@@ -147,4 +147,4 @@ CHANGE_NAME_MAP = {
     "cancel": "Анн.",
 }
 
-JOURNAL_ADDRESS = r"C:\Users\n.nikiforov\Documents\Тестовые ИИ\Журнал регистрации ИИ.xlsx"
+JOURNAL_ADDRESS = r"\\aep-dc\so\Журнал регистрации ИИ\Журнал регистрации ИИ.xlsx"
